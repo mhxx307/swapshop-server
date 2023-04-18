3949198997aa0504abf3d8a0ad1156fd495b0c57
mhxx307 <minhquan.lavo@gmail.com>
2023-04-18 23:47:37 +0700

    fix: 🐛 emailjs transporter production

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -7,15 +7,15 @@ const sendEmail = async (to: string, html: string, subject: string) => {
     // create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        port: 465,
+        secure: true, // true for 465, false for other ports
         auth: {
             user: 'secondchancemarketvn@gmail.com', // generated ethereal user
             pass: process.env.EMAIL_APP_PASSWORD, // generated ethereal password
         },
-        tls: {
-            rejectUnauthorized: false, // avoid NodeJs self signed certificate error
-        },
+        // tls: {
+        //     rejectUnauthorized: false, // avoid NodeJs self signed certificate error
+        // },
     });
 
     // send mail with defined transport object
